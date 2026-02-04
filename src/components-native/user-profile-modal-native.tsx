58ed8fcdc07b035d297c5fc3badd4920ae537614
salvanastasia <anastasiasalvatore@gmail.com>
2026-02-04 12:09:02 +0100

    Remove extra closing View tag

--- src/components-native/user-profile-modal-native.tsx
+++ src/components-native/user-profile-modal-native.tsx
@@ -136,7 +136,6 @@ export default function UserProfileModal({ user, onClose, onFriendRequest, onWal
               </View>
             </View>
           </View>
-        </View>
       </View>
     </Modal>
   );
